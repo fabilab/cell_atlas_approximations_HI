a95be3a4da704e1d254e7bb0e6eb95b7e8f82c4f
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-06-16 19:37:41 +1000

    Refactor to have props instead of imperatives

--- src/components/MainBoard.js
+++ src/components/MainBoard.js
@@ -1,42 +1,103 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import ChatBox from './ChatBox';
 import PlotBox from './PlotBox';
+import transpose from "../utils/math";
 
-function MainBoard() {
+const MainBoard = () => {
 
-    const chatBox = useRef();
-    const plotBox = useRef();
-    const [stale, setStale] = useState(false);
+    const [userInstructions, setUserInstructions] = useState([]);
+    // Initially "empty", so nothing is plotted (see the render return below)
+    const [plotState, setPlotState] = useState({
+        intent: "",
+        plotType: "",
+        organism: "",
+        organ: "",
+        features: [],
+        transforms: [],
+        data: {
+            type: "", 
+            xaxis: [],
+            yaxis: [],
+            values: [],
+            valueUnit: "",
+        },
+    });
 
-    // run this function to update User's 
+    // update plot upon new NLP instruction
     useEffect(() => {
-        if (!stale)
+        if (userInstructions.length === 0)
             return;
+        const latestResponse = userInstructions.slice(-1)[0].response;
+        if (latestResponse.plot)
+            updatePlotState(latestResponse);
+        return;
+    }, [userInstructions]);
 
-        const latestResponse = chatBox.current.getUserInstructions().slice(-1)[0].response;
-        plotBox.current.updateFromNLP(latestResponse);
-        setStale(false);
+    // update chat box based on plot, other clicks, etc.
+    useEffect(() => {
+        console.log("plot state has changed");
+    }, [plotState]);
 
-    }, [stale])
+    const updatePlotState = (response) => {
+        (async () => {
+            console.log("within async, response:");
+            console.log(response);
+            const organism = response.data.organism;
+            const organ = response.data.organ;
+            const features = response.data.features;
+            const celltypes = await window.atlasapproxAPI(
+                "celltypes", { organism: organism, organ: organ });
+
+            const intent = response.intent;
+            const generalIntent = intent.split(".")[0];
+
+            let matrix;
+            if (generalIntent === "average")
+                matrix = response.data.average;
+            else if (generalIntent === "fraction_detected")
+                matrix = response.data.fractions;
+            matrix = transpose(matrix);
+
+            const plotType = "heatmap";
+            const newPlotState = {
+                intent,
+                plotType,
+                organism,
+                organ,
+                features,
+                data: {
+                    type: "matrix",
+                    xaxis: celltypes,
+                    yaxis: features,
+                    values: matrix,
+                    valueUnit: "counts per ten thousand"
+                }
+            }
+            console.log("new plot state:");
+            console.log(newPlotState);
+            setPlotState(newPlotState);
+        })();
+    };
 
     return (
         <div className="columns mb-0 pb-0 has-background-light" style={{position:"absolute",height:"100%", width:"100%"}}>
             <div className="column is-4" style={{height:'inherit'}}>
                 <ChatBox 
-                    ref={ chatBox }
-                    setParentStale={() => setStale(true)}
+                    userInstructions={ userInstructions }
+                    setUserInstructions={ setUserInstructions }
                 />
             </div>
             <div className="column is-8" id='canvas'>
                 {
                 <PlotBox
-                    ref={ plotBox }
+                    state={ plotState }
+                    setState={ setPlotState }
                 />
                 }
             </div>
         </div>
     )
-}
+};
 
 export default MainBoard;
--- src/components/PlotBox.js
+++ src/components/PlotBox.js
@@ -1,66 +1,8 @@
-import React, { useState, forwardRef, useImperativeHandle } from 'react';
+import React from 'react';
 import Heatmap from "./plots/Heatmap"
-import transpose from "../utils/math";
 
 // MainBoard.js has passed in the plotState as props
-const PlotBox = forwardRef((props, ref) => {
-    
-    // Initially "empty", so nothing is plotted (see the render return below)
-    const [state, setState] = useState({
-        intent: "",
-        plotType: "",
-        organism: "",
-        organ: "",
-        features: [],
-        transforms: [],
-        data: {
-            type: "", 
-            xaxis: [],
-            yaxis: [],
-            values: [],
-            valueUnit: "",
-        },
-    })
-
-    useImperativeHandle(ref, () => ({updateFromNLP: (response) => {
-        (async () => {
-            console.log("within async, response:");
-            console.log(response);
-            const organism = response.data.organism;
-            const organ = response.data.organ;
-            const features = response.data.features;
-            const celltypes = await window.atlasapproxAPI("celltypes", { organism: organism, organ: organ });
-
-            const intent = response.intent;
-            const generalIntent = intent.split(".")[0];
-
-            let matrix;
-            if (generalIntent === "average")
-                matrix = response.data.average;
-            else if (generalIntent === "fraction_detected")
-                matrix = response.data.fractions;
-            matrix = transpose(matrix);
-
-            const plotType = "heatmap";
-            const newPlotState = {
-                intent,
-                plotType,
-                organism,
-                organ,
-                features,
-                data: {
-                    type: "matrix",
-                    xaxis: celltypes,
-                    yaxis: features,
-                    values: matrix
-                }
-            }
-            console.log("new plot state:");
-            console.log(newPlotState);
-            setState(newPlotState);
-        })();
-    }}), []);
-
+const PlotBox = ({ state, setState }) => {
     if (state.plotType === 'heatmap') {
         return (
             <Heatmap
@@ -76,6 +18,6 @@ const PlotBox = forwardRef((props, ref) => {
     } else {
         return <></>
     }
-})
+};
 
 export default PlotBox
