b17ea460813a82ba1ed9771e6145328e003e527c
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-06-16 10:52:30 +1000

    Moved things around to keep state down

--- src/components/PlotBox.js
+++ src/components/PlotBox.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect, forwardRef } from 'react';
+import React, { useState, forwardRef, useImperativeHandle } from 'react';
 import Heatmap from "./plots/Heatmap"
+import transpose from "../utils/math";
 
 // MainBoard.js has passed in the plotState as props
 const PlotBox = forwardRef((props, ref) => {
     
+    // Initially "empty", so nothing is plotted (see the render return below)
     const [state, setState] = useState({
-        intent: "average",
-        plotType: "heatmap",
+        intent: "",
+        plotType: "",
         organism: "",
         organ: "",
         features: [],
@@ -16,9 +18,48 @@ const PlotBox = forwardRef((props, ref) => {
             xaxis: [],
             yaxis: [],
             values: [],
-            valueUnit: "counts per ten thousand",
+            valueUnit: "",
         },
     })
+
+    useImperativeHandle(ref, () => ({updateFromNLP: (response) => {
+        (async () => {
+            console.log("within async, response:");
+            console.log(response);
+            const organism = response.data.organism;
+            const organ = response.data.organ;
+            const features = response.data.features;
+            const celltypes = await window.atlasapproxAPI("celltypes", { organism: organism, organ: organ });
+
+            const intent = response.intent;
+            const generalIntent = intent.split(".")[0];
+
+            let matrix;
+            if (generalIntent === "average")
+                matrix = response.data.average;
+            else if (generalIntent === "fraction_detected")
+                matrix = response.data.fractions;
+            matrix = transpose(matrix);
+
+            const plotType = "heatmap";
+            const newPlotState = {
+                intent,
+                plotType,
+                organism,
+                organ,
+                features,
+                data: {
+                    type: "matrix",
+                    xaxis: celltypes,
+                    yaxis: features,
+                    values: matrix
+                }
+            }
+            console.log("new plot state:");
+            console.log(newPlotState);
+            setState(newPlotState);
+        })();
+    }}), []);
 
     if (state.plotType === 'heatmap') {
         return (
--- src/components/plots/Heatmap.js
+++ src/components/plots/Heatmap.js
@@ -1,16 +1,13 @@
 import CanvasXpressReact from 'canvasxpress-react';
 
-// Source code: https://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript
-function transpose(matrix) {
-    return matrix[0].map((col, c) => matrix.map((row, r) => matrix[r][c]));
-}
-
 // props: arguments passed into React components
 const Heatmap = (props) => {
     const target = props.target;
     const expression = props.values;
     const x_label = props.xaxis;
     const y_label = props.yaxis;
+
+    console.log("making heatmap");
 
     var data =  {
         "y" : {
@@ -28,7 +25,8 @@ const Heatmap = (props) => {
     };
 
     return (
-        <CanvasXpressReact target={target} data={data} config={config} width={500} height={500} />
+        <div><img src="https://fabilab.org/images/logo2.png"></img></div>
+        //<CanvasXpressReact target={target} data={data} config={config} width={500} height={500} />
    )
     
 
