bce08ee93c09156488a69fdb3a2709690c0f5781
Ying Xu <yingxu0928@gmail.com>
2023-06-29 10:21:13 +1000

    Updated style of celltypexorgan table, give the cell a green tick when value is true

--- src/components/TableBox.js
+++ src/components/TableBox.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import { Space, Table, Tag } from 'antd';
-import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+import { CheckCircleTwoTone, StopTwoTone } from '@ant-design/icons';
+import { render } from '@testing-library/react';
 
+// Showing cell types and their detection status in different organs.
 const TableBox = ({ state }) => {
-    console.log("value is !!!!!=====" + state.detected);
+    // console.log("value is"  + state.detected);
     const columns = [
       {
-        title: 'Cell Type',
+        title: 'Cell Types',
         dataIndex: 'celltype',
-        key: 'celltype'
+        key: 'celltype',
+        render: (text) => <a href={`https://www.google.com/search?q=${text}`} target="_blank">{text}</a>,
+        width: '18%',
       },
       ...state.organs.map((organ, index) => ({
         title: organ,
         dataIndex: organ,
-        render: (value) => (value ? <CheckOutlined /> : <CloseOutlined />)
+        // for each column(organ), check the detected status
+        render: (value) => (value ? <i className="fas fa-check fa-xl" style={{ color: '#44bf22' }}></i> : <StopTwoTone twoToneColor="#d9d9d9" />)
       }))
     ];
   
     const data = state.celltypes.map((celltype, index) => {
       const row = { key: index, celltype };
-      state.organs.forEach((organ, organIndex) => {
+      state.organs.map((organ, organIndex) => {
         row[organ] = state.detected[index][organIndex];
       });
       return row;
     });
   
-    return <Table columns={columns} dataSource={data} />;
+    return <Table 
+            columns={columns} 
+            dataSource={data} 
+            pagination={{
+              pageSize: 50,
+            }}
+            scroll={{
+              y: "70vh",
+            }}
+          />;
   };
   
   export default TableBox;
