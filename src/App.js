54de83201bb99f8b758ded25f19b91ae4562afc6
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-08-16 11:46:58 +1000

    Use custom domain on gh-pages

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import Navbar from './components/Navbar';
 
 function App() {
 
+  // basename is '/' when the home page is rigged up to a custom domain
+  // (e.g. atlasapprox.org). If using the default GitHub pages URL, the
+  // URL is https://fabilab.github.io/cell_atlas_approximations_HI,
+  // therefore the basename should be '/cell_atlas_approximations_HI/'
+  // It's a little annoying to have to switch that statically here.
   return (
-    <HashRouter basename='/cell_atlas_approximations_HI/'>
+    <HashRouter basename='/'>
       <div>
         <Navbar/>
         <Routes>
