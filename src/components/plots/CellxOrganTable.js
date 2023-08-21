c8462ce5d1d2e048f811c50bbf5c84747c3e774b
Ying <yingxu0928@gmail.com>
2023-08-21 12:05:11 +1000

    Updated code in cell type x organs

--- src/components/plots/CellxOrganTable.js
+++ src/components/plots/CellxOrganTable.js
@@ -6,41 +6,41 @@ const CellxOrganTable = ({ state }) => {
   
     // Filter cell types detected in multiple organs
     const multiOrgansData = state.celltypes.map((celltype, index) => {
-        const row = { key: index, celltype };
-        let organCount = 0;
-        state.organs.forEach((organ, organIndex) => {
-            row[organ] = state.detected[index][organIndex] > 0;
-            if (state.detected[index][organIndex]) {
-                organCount++;
-            }
-        });
-        return organCount > 1 ? row : null;
+      const row = { key: index, celltype };
+      let organCount = 0;
+      state.organs.forEach((organ, organIndex) => {
+        row[organ] = state.detected[index][organIndex] > 0;
+        if (state.detected[index][organIndex]) {
+            organCount++;
+        }
+      });
+      return organCount > 1 ? row : null;
     }).filter(Boolean);
 
     // Construct a dictionary for cell types uniquely detected in each organ
     const uniqueOrganDict = {};
     state.organs.forEach((organ) => {
-        uniqueOrganDict[organ] = [];
+      uniqueOrganDict[organ] = [];
     });
 
     state.celltypes.forEach((celltype, index) => {
-        let detectedCount = 0;
-        let detectedOrgan = null;
-        state.organs.forEach((organ, organIndex) => {
-            if (state.detected[index][organIndex] > 0) {
-                detectedCount++;
-                detectedOrgan = organ;
-            }
-        });
-        if (detectedCount === 1) {
-            uniqueOrganDict[detectedOrgan].push(celltype);
+      let detectedCount = 0;
+      let detectedOrgan = null;
+      state.organs.forEach((organ, organIndex) => {
+        if (state.detected[index][organIndex] > 0) {
+          detectedCount++;
+          detectedOrgan = organ;
         }
+      });
+      if (detectedCount === 1) {
+        uniqueOrganDict[detectedOrgan].push(celltype);
+      }
     });
 
     // Add a row for uniquely detected cell types at the bottom
     const uniqueRow = { key: 'unique', celltype: 'Unique Cell Types' };
     state.organs.forEach((organ) => {
-        uniqueRow[organ] = uniqueOrganDict[organ].join(', ');
+      uniqueRow[organ] = uniqueOrganDict[organ].join(', ');
     });
     multiOrgansData.push(uniqueRow);
 
