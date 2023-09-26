ebb58753e911f6ad745a8202826aec0a25c0e702
Ying <yingxu0928@gmail.com>
2023-09-26 12:26:42 +1000

    allow user to copy previous message when they click on the copy icon

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,33 @@
-import { Row, Col } from "antd";
+import { Row, Col, Tooltip } from "antd";
 import { Link } from "react-router-dom";
 import bot from "../asset/bot.png";
 import Typewriter from "typewriter-effect";
+import { CopyOutlined } from '@ant-design/icons';
+import { useState } from "react";
 
 const Message = (props) => {
   const { role, message, pause, help, setCurrentMessage } = props;
+  const [copyStatus, setCopyStatus] = useState('not-copied');
   let formattedMessage = message;
   formattedMessage = formattedMessage.replace(/,([^ ])/g, ', $1');
 
-  
+  const handleCopyText = async (textToCopy) => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      setCopyStatus('copied');
+      setTimeout(() => setCopyStatus('not-copied'), 1500);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  const renderTooltipContent = () => {
+    if (copyStatus === 'copied') {
+      return "✓ copied";
+    }
+    return null;
+  };
+
   const profileStyle = {
     width: '26px',
     height: '26px',
@@ -16,15 +35,24 @@ const Message = (props) => {
   }
 
   const messageBoxStyle = {
-    fontFamily: 'Sans-Serif',
-    fontSize: '13.5px',
-    padding: '12px',
-    borderRadius: '8px',
+    fontSize: '13px',
     backgroundColor: role === 'user' ? '#565C5E' : 'white',
     color: role === 'user' ? 'white' : '#565C5E',
     marginLeft: role === 'user' ? '0px' : '10px',
-    width: '88%',
+    position: 'relative',
+    boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)',
+    padding: '10px',
+    borderRadius: '6px',
+    marginLeft: role === 'user' ? '15px' : '0px',
+    marginRight: role === 'user' ? '0px' : '15px',
   }
+
+  const iconStyle = {
+    position: 'absolute',
+    top: '5px',     // Adjust as needed
+    right: '5px',    // Adjust as needed
+    fontSize:'12px'
+  };
 
   return (
     <Row 
@@ -37,11 +65,18 @@ const Message = (props) => {
     >
       {(role !== 'user') && <Col span={2}><img src={bot} alt="Bot Icon" style={profileStyle} /></Col>}
       {(role === 'user') ? (
-        <Col span={21} offset={3}> {/* This will offset user's message to the right */}
+      <Col span={21} offset={3}> {/* This will offset user's message to the right */}
           <div style={messageBoxStyle}>
             {formattedMessage}
+            {copyStatus === 'copied' ? 
+              <Tooltip title={renderTooltipContent()}>
+                <CopyOutlined style={iconStyle} />
+              </Tooltip> 
+              :
+              <CopyOutlined onClick={() => handleCopyText(formattedMessage)} style={iconStyle} />
+            }
           </div>
-        </Col>
+      </Col>
       ) : (
         <Col span={22}>
           <div style={messageBoxStyle}>
