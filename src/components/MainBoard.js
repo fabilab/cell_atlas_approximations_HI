b12ab2f8186adaddd48a06690f57748c5a358aef
Ying <yingxu0928@gmail.com>
2023-11-28 13:52:02 +1100

    pop up conformation modal when user refresh  the page

--- src/components/MainBoard.js
+++ src/components/MainBoard.js
@@ -1,27 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { ChatProvider } from './ChatContext'; 
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ChatBox from './ChatBox';
 import PlotBox from './PlotBox';
 import { triggersPlotUpdate } from '../utils/chatSideEffects';
 import { updatePlotState } from '../utils/updatePlotState';
+import { Modal, Button } from 'antd';
 
 const MainBoard = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const firstQuery = location.state;
   const [chatHistory, setChatHistory] = useState(null);
   const [currentResponse, setCurrentResponse] = useState(null);
   const [plotState, setPlotState] = useState({"hasLog": false});
+  const [confirmationModal, setConfirmationModal] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
-
     window.addEventListener('resize', handleResize);
+
     return () => {
       window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      setConfirmationModal(true);
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload); // page refresh
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handlePageBack = (event) => {
+      event.preventDefault();
+      console.log("clicking button back..");
+      setConfirmationModal(true);
+    };
+
+    window.addEventListener('popstate', handlePageBack); // back button
+
+    return () => {
+      window.removeEventListener('popstate', handlePageBack);
     };
   }, []);
 
@@ -30,6 +60,13 @@ const MainBoard = () => {
       updatePlotState(currentResponse, plotState, setPlotState);
     } 
   }, [currentResponse]);
+
+  const handleLeavePage = (decision) => {
+    setConfirmationModal(false);
+    if (decision) {
+      navigate('/');
+    }
+  };
 
   return (
     <ChatProvider>
@@ -46,6 +83,14 @@ const MainBoard = () => {
             {plotState && <PlotBox state={plotState} />}
           </div>
       </div>
+      <Modal
+        title="Leave Page Confirmation"
+        open={confirmationModal}
+        onOk={() => handleLeavePage(true)}
+        onCancel={() => handleLeavePage(false)}
+      >
+        <p>If you leave the page, everything will be lost. Are you sure?</p>
+      </Modal>
     </ChatProvider>
   );
 };
