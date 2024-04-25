0e52fb2dfef190aca9798b00c4d6779c590d460a
Ying <yingxu0928@gmail.com>
2024-04-25 16:17:58 +1000

    move code block of error handling to a new file

--- src/utils/chatSideEffects.js
+++ src/utils/chatSideEffects.js
@@ -3,6 +3,7 @@ import { buildAPIParams, buildAnswer } from "./nlpHelpers.js";
 import { handleNoApiIntents } from "./hanleNoApiIntent.js";
 import { handlePlotConversion } from "./plotConversion.js";
 import { handleAddRemove } from "./addRemoveHandler.js";
+import { handleErrors } from "./errorHandler.js"; 
 
 // updatePlotIntents: An array of intents that trigger a plot update.
 // These intents require either fetching data from the API or from previous plot state, and updating the plot accordingly.
@@ -287,86 +288,10 @@ export const updateChat = async (response, plotState) => {
     // ------------------- END BUILDING ANSWER WHEN MAIN API CALL SUCCEEDS ------------------- //
     
   } catch ({ status, message, error }) {
-    let errorValue = error.invalid_value;
-    let errorParam = error["invalid_parameter"];
-    let mParam = error["missing_parameter"];
-    if (error.type === "missing_parameter") {
-      switch (mParam) {
-        case "organism":
-          answer += "What species would you like to look at?";
-          break;
-        case "organ":
-          answer += "What organ would you like to look at?";
-          break;
-        case "features":
-          answer += "Please list the features (e.g. genes) you would like to look into.";
-          break;
-        case "celltype":
-          answer += "Which cell type?";
-          break;
-        case "organ^celltype":
-          answer +=
-            "I did not recognise a cell type or an organ. Please repeat the question specifying either one (but not both).";
-          // FIXME: this requests to repeat the question, so we should reset the bot
-          break;
-        default:
-          break;
-      }
-    } else if (error.type === "invalid_parameter") {
-      // invalid gene, we can auto remove it and re-call api
-      if (errorParam === "features") {
-        if (typeof params.features === "string") {
-          if (mainIntent === "comeasurement") {
-            if (errorValue.length === 1) {
-              answer += `The gene "${errorValue}" is not valid. Please ensure that gene names are spelled correctly.`;
-            } else if (errorValue.length === 2) {
-              answer += `The genes "${errorValue}" are not valid. Please ensure that gene names are spelled correctly.`;
-            }
-            apiData = null;
-          } else {
-            params.features = params.features
-              .split(",")
-              .filter((feature) => !errorValue.includes(feature.toLowerCase()))
-              .join(",");
-          }
-        } else {
-          // example case: markers of myopeptidocyte in s_lacustris whole
-          // remove marker genes that contain a space
-          params.features = params.features.filter(
-            (feature) => !feature.includes(" ")
-          );
-          //  re call the extra endpoint which is dotplot
-          endpoint = "dotplot";
-        }
-
-        if (mainIntent !== "comeasurement") {
-          if (params.features.length !== 0) {
-            apiData = await atlasapprox[endpoint](params);
-            if (mainIntent === "markers") {
-              apiData["markers"] = params.features;
-              apiData["celltype"] = entities.filter(
-                (e) => e.entity === "celltype"
-              )[0].sourceText;
-            }
-            answer += `Invalid features detected: "${errorValue}". These have been automatically excluded<br>`;
-            answer += buildAnswer(intent, plotState, apiData);
-            answer += `<br><br>It covers ${apiData.celltypes.length} cell types and ${apiData.features.length} genes.`;
-          } else {
-            answer = `The feature "${errorValue}" is not available in our current dataset. Are you sure it is spelled correctly? You can retry the question with a different feature if you like.`;
-          }
-        }
-      } else if (errorParam === "feature") {
-        answer += `The feature "${errorValue}" is not available. Have you checked the spelling of that feature? You can retry with a different fgeature if you like.`;
-      } else if (errorParam === "organism") {
-        answer += `Organism "${errorValue}" is not available. Could it be a spelling error? Otherwise, you can try a different species.`;
-      } else if (errorParam === "organ") {
-        answer += `Organ "${errorValue}" is not available in this organism. Some organisms (e.g. coral) have been dissociated as "whole", without separating the organs. You can try asking "What organs are available for <organism>?" to get the list of organs for this organism.`;
-      } else if (errorParam === "celltype") {
-        answer += `Cell type "${errorValue}" is not available in this context. If you are looking into a specific organ, you can ask "What cell types are available in <organism> <organ>?", otherwise you can draw a table of cell types and organs in that organism by asking "Show cell type by organ table of <organism>".`;
-      }
-    } else {
-      answer = message;
-    }
+    const result = await handleErrors(error, mainIntent, params, entities, answer, endpoint, plotState, intent, message);
+    params = result.params;
+    apiData = result.apiData;
+    answer = result.answer
   } finally {
     return {
       hasData: apiData !== null,
