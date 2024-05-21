5a2173bdbece55b29c9d17a83c55a5bb4ed62b0f
Ying <yingxu0928@gmail.com>
2024-05-21 10:40:32 +1000

    Show unit and title on highest measurement plot based on plot type

--- src/components/plots/BarChart.js
+++ src/components/plots/BarChart.js
@@ -14,16 +14,12 @@ const BarChart = ({ state }) => {
   let title = '';
   let yLabel = '';
   switch (plotType) {
-    case "similar_celltypes.geneExpression":
+    case "similarCelltypes":
       average = average.map((x) => x * scaleFactor);
       title = `<b>Cell type similarity to ${targetCelltype} via gene expression correlation</b>`;
-      yLabel = `Distance (x${scaleFactor})`;
+      yLabel = `Scaled distance`;
       break;
-    case "highest_measurement.geneExpression":
-      title = `<b>Highest expressor of <a href="https://www.genecards.org/cgi-bin/carddisp.pl?gene=${features}" target="_blank">${features}</a> gene in ${organism}</b>`;
-      yLabel = unit;
-      break;
-    case "highest_measurement.chromatinAccessibility":
+    case "highestMeasurement":
       title = `<b>Highest expressor of ${features} in ${organism}</b>`;
       yLabel = unit;
       break;
@@ -51,6 +47,7 @@ const BarChart = ({ state }) => {
 
   let data = [trace1];
 
+  console.log(yLabel);
   let layout = {
     width: '100%',
     height: '100%',
