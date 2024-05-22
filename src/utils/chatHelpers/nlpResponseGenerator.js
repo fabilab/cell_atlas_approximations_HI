3adc3a1ba56210cf2a8fb37df68dbd4d40cd1988
Ying <yingxu0928@gmail.com>
2024-05-22 11:52:26 +1000

    Add a button to the modebar to enable download of CSV data

--- src/utils/chatHelpers/nlpResponseGenerator.js
+++ src/utils/chatHelpers/nlpResponseGenerator.js
@@ -170,6 +170,7 @@ const buildAnswer = (intent, plotState, data = null) => {
         break;
       case "homologs":
         answer = "The homologos of " + data.features + " from " + data.source_organism + " to " + data.target_organism + " are shown in the bipartite graph.";
+        answer += "<br><br>Use the top right mode bar to download plot or download data in CSV format";
         break;
       case "organxorganism":
         answer = "The presence of <b>" + data.celltype + " </b>cells across different organs and species is shown in the table.";
