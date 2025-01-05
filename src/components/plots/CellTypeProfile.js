9550e30156fa8aaad1a8f91e11787c83bbd44b14
Ying <yingxu0928@gmail.com>
2025-01-05 17:12:21 +1100

    show marker genes for hovered organ in cell type profile

--- src/components/plots/CellTypeProfile.js
+++ src/components/plots/CellTypeProfile.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import atlasapprox, { measurement_types } from "@fabilab/atlasapprox";
 import ImageMapper from "react-img-mapper";
 import orgMeta from "../../utils/organismMetadata.js";
+import transpose from "../../utils/plotHelpers/math";
+import BubbleHeatmap from './BubbleHeatmap';
 import { scaleImage } from "../../utils/plotHelpers/scaleImage.js";
+import { Typography } from "antd";
 import { scaleLinear } from "d3-scale";
 import Plot from 'react-plotly.js';
 
+const { Text } = Typography;
+
 const CellTypeProfile = ({ state }) => {
   const { cellType, description, distributionData } = state;
-  console.log(distributionData)
   const [selectedSpecies, setSelectedSpecies] = useState('all');
+  const [hoveredOrgan, setHoveredOrgan] = useState(null);
+  const [selectedOrganMarkers, setSelectedOrganMarkers] = useState(null);
+  const [markerExpressionPlotData, setMarkerExpressionPlotData] = useState(null);
   const [organTotal, setOrganTotal] = useState(null);
   const [distributionPlotData, setDistributionPlotData] = useState(null);
   const [scalingFactors, setScalingFactors] = useState({ width: 1, height: 1 });
@@ -41,10 +49,8 @@ const CellTypeProfile = ({ state }) => {
     }));
     
     if (plotData.length === 1) {
-      console.log(plotData[0]['y'])
       const minCount = Math.min(...plotData[0]['y']);
       const maxCount = Math.max(...plotData[0]['y']);
-      console.log(minCount, maxCount)
       const colorScale = scaleLinear()
         .domain([minCount, maxCount])
         .range(["#f0d2cc", "#ed4e2b"]);
@@ -56,6 +62,37 @@ const CellTypeProfile = ({ state }) => {
     setOrganTotal(total);
     setDistributionPlotData(plotData)
   }
+
+  const handleMouseEnter = async (area) => {
+    if (area.name.includes("-label")) return;
+    setHoveredOrgan(area.name);
+    try {
+      const marker_params = {
+        organism: selectedSpecies,
+        organ: area.name,
+        celltype: cellType,
+        number: 5,
+        measurement_types: "gene_expression"
+      }
+
+      let apiResponse = await atlasapprox.markers(marker_params);
+      let markers = apiResponse.markers;
+      setSelectedOrganMarkers(markers)
+
+      apiResponse = await atlasapprox.dotplot({
+        organism: selectedSpecies,
+        celltype: cellType,
+        features: markers,
+        measurement_types: "gene_expression",
+      })
+
+      setMarkerExpressionPlotData(apiResponse);
+
+
+    } catch (error) {
+      console.error("Error fetching markers:", error);
+    }
+  };
 
   // Render image map component
   const renderImageMap = () => {
@@ -98,12 +135,27 @@ const CellTypeProfile = ({ state }) => {
       <ImageMapper
         src={require(`../../asset/anatomy/${imagePathPrefix}.jpg`)}
         map={{ name: `${selectedSpecies}-map`, areas: areas }}
-        // onMouseEnter={handleMouseEnter}
+        onMouseEnter={handleMouseEnter}
         width={450}
         height={450}
       />
     );
   }
+
+  const renderColorBar = () => (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginLeft: "10px" }}>
+      <Text>High</Text>
+      <div
+        style={{
+          height: "100px",
+          width: "20px",
+          background: `linear-gradient(to bottom, #ed4e2b, #f0d2cc)`,
+          margin: "10px 0",
+        }}
+      />
+      <Text>Low</Text>
+    </div>
+  );
 
   useEffect(() => {
     if (!distributionPlotData) {
@@ -113,6 +165,8 @@ const CellTypeProfile = ({ state }) => {
 
   useEffect(() => {
     if (distributionPlotData) {
+      setHoveredOrgan(null);
+      setSelectedOrganMarkers(null);
       if (selectedSpecies === 'all') {
         makePlot(distributionData.data)
       } else {
@@ -220,12 +274,55 @@ const CellTypeProfile = ({ state }) => {
           }}
         />
       </div>
-      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+      <div style={{ padding: "0% 4%", display: "flex", justifyContent: "space-between" }}>
+        <div style={{ display: "flex", alignItems: "flex-start" }}>
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+              {
+                selectedSpecies !== 'all' &&
+                renderImageMap()
+              }
+          </div>
           {
             selectedSpecies !== 'all' &&
-            renderImageMap()
+            renderColorBar()
           }
+        </div>
+        <div style={{ flex: 1, overflow: "auto", minWidth: "0", paddingLeft: "5%" }}>
+          {
+            hoveredOrgan && selectedOrganMarkers && 
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+              <h4>Top 5 marker genes for {cellType} in {selectedSpecies} {hoveredOrgan}</h4>
+              <table>
+                {
+                  selectedOrganMarkers.map(m => 
+                    <tr><td>{m}</td></tr>
+                  )
+                }
+              </table>
+            </div>
+          }
+        </div>
       </div>
+      {/* {
+            hoveredOrgan && selectedSpecies && markerExpressionPlotData &&
+            <BubbleHeatmap
+              state={{
+                plotType: "fractionDetected",
+                xaxis: markerExpressionPlotData.organs,
+                yaxis: markerExpressionPlotData.features,
+                average: transpose(markerExpressionPlotData.average),
+                fractions: transpose(markerExpressionPlotData.fraction_detected),
+                organism: selectedSpecies,
+                organ: markerExpressionPlotData.organs,
+                celltype: cellType,
+                unit: markerExpressionPlotData.unit,
+                hasLog: false,
+                measurement_type: markerExpressionPlotData.measurement_type,
+                queriedGenes: markerExpressionPlotData.features,
+                isSurface: false
+              }}
+            />
+          } */}
     </div>
   );
 };
